dc753763bd931b3ff27eb4cdfaa236fea32186ec
Pawel Pasich <pawel.pasich@jgbm.co.uk>
2023-06-19 00:17:21 +0100

    adding run.sh arguments

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-docker run --detach   --hostname 10.0.0.50 --publish 10.0.0.50:4443:443   --publish 10.0.0.50:4480:80   --publish 10.0.0.50:4422:22   --name gitlab2   --volume /home/fox/Docker/gitlab3/config:/etc/gitlab   --volume /home/fox/Docker/gitlab3/logs:/var/log/gitlab   --volume /home/fox/Docker/gitlab3/data:/var/opt/gitlab   --shm-size 256m   gitlab/gitlab-ce:latest
+read -p 'Hostname [10.0.0.50]: ' HOSTNAME
+HOSTNAME=${HOSTNAME:-10.0.0.50}
+read -p 'Name: [GitLab.local]' NAME
+NAME=${NAME:-GitLab.local}
+read -p 'Port 22 [4422]: ' PORT1
+PORT1=${PORT1:-4422}
+read -p 'Port 80 [4480]: ' PORT2
+PORT2=${PORT2:-4480}
+read -p 'Port 443 [4443]: ' PORT3
+PORT3=${PORT3:-4443}
+docker run --detach --hostname $HOSTNAME --publish $HOSTNAME:$PORT3:443 --publish $HOSTNAME:$PORT2:80 --publish $HOSTNAME:$PORT1:22 --name $NAME --volume ./config:/etc/gitlab --volume ./logs:/var/log/gitlab --volume ./data:/var/opt/gitlab --shm-size 256m gitlab/gitlab-ce:latest
